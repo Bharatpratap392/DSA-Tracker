6d84179ebdc7b3e21e0fc1b60f5a4e2df8754829
ELDER <bharatpratap12abh@gmail.com>
2025-07-31 11:39:22 +0530

    your message

---
+++ 100 Questions Logics of c++/Q-2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a >= b && a >= c)
+    {
+        cout << "a is greater " << endl;
+    }
+    else if (b >= a && b >= c)
+    {
+        cout << "b is greater" << endl;
+    }
+    else if (c >= a && c >= b)
+    {
+        cout << "c is greater";
+    }
+    return 0;
+}
