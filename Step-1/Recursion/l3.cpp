0a2171e035f1af4002559243bd4f3fdcdcc81d90
ELDER <bharatpratap12abh@gmail.com>
2025-08-28 12:48:42 +0530

    l3 completed

---
+++ Step-1/Recursion/l3.cpp
@@ -0,0 +1,37 @@
+//1.sum of all 1 to n numbers
+#include <bits/stdc++.h>
+using namespace std;
+
+void learning_recursion(int i,int sum)
+{
+    if(i<1){
+        cout<<sum;
+        return;
+    }
+    learning_recursion(i-1,sum+i);
+}
+int main() {
+    int n;
+    cin>>n;
+    learning_recursion(n,0);
+    return 0;
+}
+
+//2.Factorial of N numbers 
+#include <bits/stdc++.h>
+using namespace std;
+void learning_recursion(int i,int f){
+    if(i<1){
+        cout<<f;
+        return;
+    }
+    learning_recursion(i-1,f*i);
+    
+}
+
+int main() {
+    int n;
+    cin>>n;
+    learning_recursion(n,1);
+    return 0;
+}
