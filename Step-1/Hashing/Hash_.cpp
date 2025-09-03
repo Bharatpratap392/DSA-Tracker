e34c9d5ee4600d79092ece026ef21d579e2d688b
ELDER <bharatpratap12abh@gmail.com>
2025-09-03 12:21:35 +0530

    updates

--- Step-1/Hashing/Hash_.cpp
+++ Step-1/Hashing/Hash_.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main() {
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cout<<arr[i];
+    // Take array input
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-
-    //precompute
-    int hash[13]={0};
-    for(int i=0;i<n;i++){
-        hash[arr[i]]+=1;
+    // Precompute frequencies (hashing)
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++) {
+        hash[arr[i]] += 1;
     }
-
     int q;
-    cin>>q;
-    while(q--){
+    cin >> q;
+    while (q--) {
         int number;
-        cin>>number;
-        //fetch
-        cout<<hash[number]<<endl;
+        cin >> number;
+        // Fetch frequency
+        cout << hash[number] << endl;
     }
+    return 0;
 }
