6ef90fc087824dd6daa3cee9e0098c73c4fb2246
ELDER <bharatpratap12abh@gmail.com>
2025-09-11 00:33:40 +0530

    hash

--- Step-1/Hashing/hash_map_3.cpp
+++ Step-1/Hashing/hash_map_3.cpp
@@ -1,27 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-//counting through ordered  map
-int main() {
+// counting through ordered  map
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    map<int,int>mpp;
-    for(int i=0;i<n;i++){
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
         mpp[arr[i]]++;
     }
-    for(auto it :mpp){
-        cout<<it.first<<"->"<<it.second<<endl;
+    for (auto it : mpp)
+    {
+        cout << it.first << "->" << it.second << endl;
     }
     int q;
-    cin>>q;
-    while(q--){
+    cin >> q;
+    while (q--)
+    {
         int number;
-        cin>>number;
-        cout<<mpp[number]<<endl;
+        cin >> number;
+        cout << mpp[number] << endl;
     }
     return 0;
 }
+/*
+input
+7
+1 2 3 1 3 2 12
+5
+1
+2
+3
+4
+12
+*/
